e540fb64814269a05b59fb83f54caf0e4f5dd110
kiiramat <kiiramati@gmail.com>
2024-11-12 17:29:11 +0000

    Display computer's choice

--- js/app.js
+++ js/app.js
@@ -3,9 +3,23 @@ const userChoiceDisplay = document.getElementById("your-choice")
 const resultDisplay = document.getElementById("result")
 const possibleChoices = document.querySelectorAll("button")
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener("click", (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
 
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length)
+
+    if (randomNumber === 0) {
+        computerChoice = "rock"
+    } else if (randomNumber === 1) {
+        computerChoice = "paper"
+    } else if (randomNumber === 2) {
+        computerChoice = "scissors"
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
